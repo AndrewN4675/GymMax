16a8b58e9f8580e796b1851341c112aea96197e0
JohnnyBoiTime <74278206+JohnnyBoiTime@users.noreply.github.com>
2025-03-14 20:20:11 -0700

    Update page.tsx

--- gym_maxxing/src/app/register/page.tsx
+++ gym_maxxing/src/app/register/page.tsx
@@ -8,9 +8,9 @@ export default function Login() {
     <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
       <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-300 bg-white px-4 py-6 pt-8 text-center sm:px-16">
-          <h3 className="text-xl font-semibold text-black">Login</h3>
+          <h3 className="text-xl font-semibold text-black">Register</h3>
           <p className="text-sm text-gray-500">
-            Log in to Your GymMax Account
+            Create your Gym Max account
           </p>
         </div>
         <Form
@@ -35,14 +35,7 @@ export default function Login() {
             }
           }}
         >
-            
           <SubmitButton>Create Account</SubmitButton>
-
-          <p className="text-center text-sm text-gray-600">
-            {"Don't have an account? "}
-            <Link href="/register" className="font-semibold text-gray-800 underline"> Sign up </Link>
-            {' for free.'}
-          </p>
         </Form>
       </div>
     </div>
