ab593bdf1192bdd407d7e485a02930ea67d6023d
Derek Williams <derekwilliams505@gmail.com>
2025-03-18 11:26:58 -0700

    fixing error

--- gym_maxxing/src/app/components/StepForm.tsx
+++ gym_maxxing/src/app/components/StepForm.tsx
@@ -7,7 +7,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
 interface StepFormProps {
-  onStepsLogged: () => void;
+  onStepsLogged: (stepCount: number) => Promise<void>;
   isLoading: boolean;
   setIsLoading: (loading: boolean) => void;
 }
